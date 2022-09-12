23ff08b789532f772d4708f9909de5aa36c4cdae
Matheus Victor <matheusvictor2000@gmail.com>
2022-09-12 13:32:03 -0300

    structured header/recent work

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+(function () {
+    const menu = document.querySelector('.burguer-menu')
+    const navMenu = document.querySelector('.nav__mobile')
+    menu.addEventListener('click', () => {
+        navMenu.classList.toggle('active')
+    })
+
+    const closeMenu = document.querySelector('.close-menu')
+    closeMenu.addEventListener('click', () => {
+        navMenu.classList.remove('active')
+    })
+}())
